5f09dc43f2acc4b47adbd10fdf7970604be9bdbe
Pankaj Gurung <grgpankaj004@gmail.com>
2025-02-24 16:52:15 +0545

    update: implemented toggle dark/light mode functionality with icons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,23 @@
-import { Button } from "./components/ui/button"
-
+import {BrowserRouter, Route, Routes} from "react-router-dom" 
+import Layout from "./components/layout"
+import { ThemeProvider } from "./context/theme-provider"
+import WeatherDashboard from "./pages/weather-dashboard"
+import CityPage from "./pages/city-page"
 
 const App = () => {
   return (
     <>
-    <Button>Click me</Button>
+    <BrowserRouter>
+    <ThemeProvider defaultTheme="dark">
+        <Layout>
+          <Routes>
+            <Route path="" element={<WeatherDashboard/>}></Route>
+            <Route path="/city/:cityName" element={<CityPage/>}></Route>
+          </Routes>
+        </Layout>
+    </ThemeProvider>
+        
+    </BrowserRouter>
     </>
   )
 }
